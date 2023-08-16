1b6262a0da166b30aae71b57f15cb08a92be18d1
ngohoang <nphhoang21@clc.fitus.edu.vn>
2023-08-16 14:51:11 +0700

    image with public folder

--- test.py
+++ test.py
@@ -1,15 +1,5 @@
-A = [100,50,200,50,200]
+sum = 0
+for i in range(10):
+    sum += 0.1000000
 
-min = A[0]
-max = A[0]
-count = 0
-for i in range(1, len(A)):
-    if A[i] < min:
-        count+=1
-        min = A[i]
-
-    elif A[i] > max:
-        count+=1
-        max = A[i] 
-
-print(count)
+print(sum)

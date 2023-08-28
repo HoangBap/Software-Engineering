00b6fd738b9cb172ec7801be75d34e8335562780
ngohoang <nphhoang21@clc.fitus.edu.vn>
2023-08-28 13:40:34 +0700

    ver1.2

--- src/controllers/profileModification.js
+++ src/controllers/profileModification.js
@@ -5,8 +5,10 @@ const controller = {}
 //Update the user profile
 controller.updateprofile = async (req, res) => {
     const userID = Number(req.cookies.userID) //data stored in cookie is a string so need to be converted to int value
-    const {fullname, email, phone_number, date_of_birth, gender, home_address, country} = req.body
+    const {fullname, phone_number, date_of_birth, gender, home_address, country} = req.body
+
     console.log(fullname)
+    console.log("User trying to update the profile")
     
     const flag = await updateUserProfile(userID, fullname, phone_number, gender, home_address, country, date_of_birth)
     if(flag) {
@@ -21,14 +23,30 @@ controller.updateprofile = async (req, res) => {
 
 controller.getprofile = async (req, res) => {
     const userID = req.cookies.userID
+    const email = req.cookies.email
+
     if(!userID) {
         console.log('Can not retrieve the cookie!')
         return 
     }
 
     const user_profile = await getUserProfile(userID)
-    res.render("editprofile", {email: user_profile.email, fullname: user_profile.fullname, phone_number: user_profile.phone_number, gender: user_profile.gender, home_address: user_profile.home_address, country: user_profile.country, date_of_birth: user_profile.date_of_birth})
+    res.render("edit_profile", {email: email, fullname: user_profile.fullname, phone_number: user_profile.phone_number, gender: user_profile.gender, home_address: user_profile.home_address, country: user_profile.country, date_of_birth: user_profile.date_of_birth})
     return 
 }
 
+controller.viewprofile = async(req, res) => {
+    const userID = req.cookies.userID
+    const email = req.cookies.email
+
+    if(!userID) {
+        console.log('Cannot retrieve the cookie!')
+        return
+    }
+    console.log('UserID: ', userID)
+
+    const user_profile = await getUserProfile(userID)
+    res.render("profile")
+    return
+}
 export default controller
--- src/routes/profileModification.js
+++ src/routes/profileModification.js
@@ -3,6 +3,8 @@ import profile_controller from '../controllers/profileModification.js'
 
 const router = express.Router()
 
+router.get('/profile', profile_controller.viewprofile)
+
 router
     .route('/editprofile')
     .get(profile_controller.getprofile)

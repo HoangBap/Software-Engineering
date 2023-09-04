43c78748e8599fdeb1085f319d64e9755ea1b2e7
SplendidBoy1 <102167808+SplendidBoy1@users.noreply.github.com>
2023-09-04 20:42:39 +0700

    add_js_login

---
+++ src/publics/scripts/login.js
@@ -0,0 +1,24 @@
+let Email = document.getElementById('email')
+let Password = document.getElementById('password')
+
+let message = document.getElementById('message')
+
+fetch ('/login', {
+    method: 'POST',
+    body: JSON.stringify({email: Email.value, password: Password.value}),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+})
+.then(res =>{
+    return res.json()
+})
+.then(data =>{
+    if (data.flag == false){
+        message.style.display = "block"
+    }
+    else{
+        message.style.display = "none"
+    }
+})
+
